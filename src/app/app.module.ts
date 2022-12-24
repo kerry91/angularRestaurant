f50c8344568605345b253d245e1c60b2fbd5c1a5
kerry91 <kerry91warnes@gmail.com>
2022-12-24 18:48:23 +0000

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -48,6 +48,7 @@ import { baseURL } from './shared/baseurl';
 import { PromotionService } from './services/promotion.service';
 
 import { ProcessHTTPMsgService } from './services/process-httpmsg.service';
+import { HighlightDirective } from './directives/highlight.directive';
 
 @NgModule({
   declarations: [
@@ -60,7 +61,8 @@ import { ProcessHTTPMsgService } from './services/process-httpmsg.service';
     HomeComponent,
     ContactComponent,
     LeaderdetailComponent,
-    LoginComponent
+    LoginComponent,
+    HighlightDirective
   ],
   imports: [
     MatListModule,
